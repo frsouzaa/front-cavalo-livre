cb4b8c82c3a16399b40c2d6077b0742565ba3f69
frsouzaa <felipe.souza1506@outlook.com>
2023-03-21 09:23:43 -0300

    feat: desenvolvimento do card de produto e da grid de produtos

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,41 @@
 function carregar_conteudo() {
-    const conteudo = document.getElementById("content");
-    for (let i = 0; i < 10; i++) {
+    const conteudo = document.getElementsByClassName("produtos")[0];
+    const linhas = 7; 
+    for (let i = 0; i < linhas; i++) {
         conteudo.innerHTML += `
         <div class="row">
-            <div class="div-card-produto col-md-3">
-                <div class="produto"></div>
-            </div>
-            <div class="div-card-produto col-md-3">
-                <div class="produto"></div>
-            </div>
-            <div class="div-card-produto col-md-3">
-                <div class="produto"></div>
-            </div>
-            <div class="div-card-produto col-md-3">
-                <div class="produto"></div>
+            ${gerar_cards(3)}
+        </div>
+        `;
+    }
+}
+
+function gerar_cards(quantidade) {
+    let cards = ``;
+    for (let i = 0; i < quantidade; i++) {
+        cards = ` ${cards}
+        <div class="div-card-produto col-md-4" onclick="abrir_produto()">
+            <div class="produto">
+                <div class="div-imagem">
+
+                </div>
+                <div class="interacao row">
+                    <div class="informacoes col-md">                                    
+                        <div class="nome-produto text-wrap fs-5">
+                            Lorem ipsum dolor sit amet consectetur
+                        </div>
+                        <div class="preco-produto text-wrap text-uppercase fs-4">
+                            R$ 0,00
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         `;
     }
+    return cards;
+}
+
+function abrir_produto() {
+    console.log("oi")
 }
